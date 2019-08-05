3e6fe4470cf9fca5b4bf9db18abf7f025c2562e2
EOP <eop@sitecore.com>
2019-08-05 12:04:54 +0300

    Added processor to check the "http" links

---
+++ src/Sitecore.Support.348219/Modules/EmailCampaign/Core/Pipelines/GenerateLink/CheckHttpLink.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Modules.EmailCampaign.Core.HostnameMapping;
+using System;
+using System.Collections.Generic;
+
+namespace Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink
+{
+    public class CheckHttpLink : GenerateLinkProcessor
+    {
+        public override void Process(GenerateLinkPipelineArgs args)
+        {
+            Assert.IsNotNull(args, "Arguments can't be null");
+            if (restrictedValues.Contains(args.Url))
+            {
+                args.Url = "";
+            }
+        }
+
+        private List<string> restrictedValues = new List<string>()
+        {
+            "http://",
+            "https://",
+            "http",
+            "https",
+            "http:",
+            "https:",
+            "http:/",
+            "https:/"
+        };
+    }
+}
